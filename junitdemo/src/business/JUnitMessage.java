d6b2eab1650f89a8feb257eac9c23c5d343e943e
itsrko02 <ritwik.tiwari02@gmail.com>
2021-08-04 16:52:10 +0530

    jUnit testing demo ver 1

---
+++ junitdemo/src/business/JUnitMessage.java
@@ -0,0 +1,23 @@
+package business;
+
+public class JUnitMessage {
+
+	String m;
+	public JUnitMessage(String msg)
+	{
+		m=msg;
+	}
+	
+	public String printMessage()
+	{
+		return m;
+	}
+
+
+
+	public String printHiMessage()
+	{
+		return "Hi "+m;
+	}
+
+}
---
+++ junitdemo/src/business/MyMath.java
@@ -0,0 +1,19 @@
+package business;
+
+public class MyMath {
+
+	public MyMath() {
+	       
+    }
+	
+	
+    public int sum(int[] numbers) {
+        int sum = 0;
+        for (int i : numbers) {
+            sum += i;
+        }
+        return sum;
+    }
+	
+	
+}
